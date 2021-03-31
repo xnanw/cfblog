7211739bc7fb353b9706986dc546191da2bcc74d
xnanw <69662838+xnanw@users.noreply.github.com>
2021-03-31 18:27:55 +0800

    Update pass.js

--- themes/default2.0/pass.js
+++ themes/default2.0/pass.js
@@ -1,5 +1,5 @@
        (function(){
-                   if (prompt('请输入密码') !== 'wmnhw'){
+                   if (prompt('请输入密码') !== 'xnan'){
                        alert('密码错误');
                        window.close();
                        window.history.go(-1);
