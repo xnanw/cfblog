6762f6ac7b4ca9b7a1380bf21d9ed2236fc716e3
xnanw <69662838+xnanw@users.noreply.github.com>
2021-03-31 18:23:56 +0800

    Add files via upload

---
+++ themes/default2.0/pass.js
@@ -0,0 +1,8 @@
+       (function(){
+                   if (prompt('请输入密码') !== 'wmnhw'){
+                       alert('密码错误');
+                       window.close();
+                       window.history.go(-1);
+                                                history.back(-1);
+                   }
+        })();
