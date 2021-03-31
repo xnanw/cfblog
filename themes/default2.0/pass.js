c67b37dab9cbe8b2e2492693f693348862ba6010
xnanw <69662838+xnanw@users.noreply.github.com>
2021-03-31 18:38:14 +0800

    Delete pass.js

--- themes/default2.0/pass.js
+++
@@ -1,8 +0,0 @@
-       (function(){
-                   if (prompt('请输入密码') !== 'xnan'){
-                       alert('密码错误');
-                       window.close();
-                       window.history.go(-1);
-                                                history.back(-1);
-                   }
-        })();
